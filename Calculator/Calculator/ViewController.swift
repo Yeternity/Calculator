4303df55b6dc886b7cd742f911cbeae2e6ac5601
seirra <hack@seirradeMac.local>
2017-04-05 19:32:31 +0800

    introduction to ios10, xcode 8 and swift 3

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,9 +12,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    var userIsInTheMiddleTyping = false
+    
     @IBAction func toughDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("\(digit) was touched")
+        //print("\(digit) was touched")
+        if userIsInTheMiddleTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleTyping = true
+        }
     }
+    
+    var displayValue: Double {
+        get  {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                //display.text = String(Double.pi)
+                displayValue = Double.pi
+            case "√":
+                //let operand = Double(display.text!)!
+                //display.text = String(sqrt(operand))
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    
 }
 
